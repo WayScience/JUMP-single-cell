d4437b67ce5745bac539e86b76737de40e1cb853
Cameron Mattson <camo@oak.ucdenver.pvt>
2023-07-21 13:04:11 -0600

    Added the scripts to scripts and path info to download plates

--- 0.download_data/download_jump_data.py
+++ 0.download_data/download_jump_data.py
@@ -1,23 +1,22 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Download sqlite plate data from aws
-# Note, this script was not rerun to display the outputs for the sake of time. To download the data, you must be signed into your aws account
-
-# ## Imports
-
-# In[ ]:
-
+r"""°°°
+# Download sqlite plate data from aws
+Note, this script was not rerun to display the outputs for the sake of time. To download the data, you must be signed into your aws account
+°°°"""
+# |%%--%%| <TM56DRUlQQ|7T0gePh3Zn>
+r"""°°°
+## Imports
+°°°"""
+# |%%--%%| <7T0gePh3Zn|XqllLfeEBZ>
 
 import subprocess
 import pathlib.Path
 
-
-# ## Find the root of the git directory
-# This allows file paths to be referenced in a system agnostic way
-
-# In[ ]:
-
+# |%%--%%| <XqllLfeEBZ|q52bES5PJV>
+r"""°°°
+## Find the root of the git directory
+This allows file paths to be referenced in a system agnostic way
+°°°"""
+# |%%--%%| <q52bES5PJV|xCbL8BuHjD>
 
 # Get the current working directory
 cwd = pathlib.Path.cwd()
@@ -36,11 +35,11 @@ else:
 if root_dir is None:
     raise FileNotFoundError("No Git root directory found.")
 
-
-# ## Download the plate sqlite data from AWS S3
-
-# In[ ]:
-
+# |%%--%%| <xCbL8BuHjD|DgPm3tAEsr>
+r"""°°°
+## Download the plate sqlite data from AWS S3
+°°°"""
+# |%%--%%| <DgPm3tAEsr|NDX1gPfGsN>
 
 # Specify the data path for downloading the data
 download_map = "jump_dataset.csv"
@@ -50,4 +49,3 @@ save_location = f"{root_dir}/big_drive"
 
 # Download the data using a bash script
 subprocess.run(["bash", "download_jump_data.sh", download_map, save_location])
-
