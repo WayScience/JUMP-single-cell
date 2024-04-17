c906811eb3f373e5ed524a6355ecd1aaea18e5a3
Cameron Mattson <camo@oak.ucdenver.pvt>
2024-04-17 15:57:21 -0600

    Determinsitcally changed preprocessing order

--- 1.process_data/process_data.sh
+++ 1.process_data/process_data.sh
@@ -5,8 +5,5 @@ py_path="nbconverted"
 # convert all notebooks to python files into the nbconverted folder
 jupyter nbconvert --to python --output-dir="${py_path}/" *.ipynb
 
-# Run all Python files in the folder
-for pfile in "$py_path"/*.py; do
-    echo "Running $pfile"
-    python3 "$pfile"
-done
+python3 "$py_path/normalize_jump_pilot.py"
+python3 "$py_path/feature_select_jump_pilot.py"
