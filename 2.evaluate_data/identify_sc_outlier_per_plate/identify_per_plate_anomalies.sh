674134f97f9ab0b853d3438af3fa24812bd0c0ea
Cameron Mattson <camo@oak.ucdenver.pvt>
2024-04-29 12:13:21 -0600

    Added pr suggestions to improve readability and summarize runtime performance

--- 2.evaluate_data/identify_sc_outlier_per_plate/identify_per_plate_anomalies.sh
+++ 2.evaluate_data/identify_sc_outlier_per_plate/identify_per_plate_anomalies.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script calls python code to identify anomalous cells by passing each plate as input.
+
+# Record the start time of the script
+script_start_time=$(date +%s.%N)
+
 # Get the root directory
 git_root=$(git rev-parse --show-toplevel)
 
@@ -10,6 +15,9 @@ jupyter nbconvert --to python --output-dir="${py_path}/" *.ipynb
 
 # Get the normalized data path (with multiple plates)
 feature_selected_data_path="${git_root}/big_drive/feature_selected_sc_data"
+
+# Track the number of plates processed
+number_plates=0
 
 # Check if the folder exists
 if [ -d "$feature_selected_data_path" ]; then
@@ -25,11 +33,12 @@ if [ -d "$feature_selected_data_path" ]; then
             # Get the plate name from the filename
             plate_name="$(ls $file | awk -F "/" '{print $NF}' | awk -F "_" '{print $1}')"
 
-            echo ""
-            echo "Processing Plate $plate_name"
+            echo -e "\nProcessing Plate $plate_name"
 
             # Generate the probabilities for the specified plate data
             python3 "$py_path/identify_anomalous_single_cells_fs.py" $file $plate_name
+
+            ((number_plates++))
 
         fi
 
@@ -39,3 +48,9 @@ if [ -d "$feature_selected_data_path" ]; then
 
     done
 fi
+
+script_end_time=$(date +%s.%N)
+
+# Show execution summary
+script_runtime=$(echo "($script_end_time - $script_start_time) / 3600" | bc)
+echo -e "\nScript processed $number_plates plates in $script_runtime hours"
