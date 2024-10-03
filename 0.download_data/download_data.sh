a89a875ad7f648ce08df46f145f99a68d382df19
d33bs <dave.bunten@cuanschutz.edu>
2024-10-03 09:58:50 -0600

    update bash script for step 0

--- 0.download_data/download_data.sh
+++ 0.download_data/download_data.sh
@@ -3,12 +3,13 @@
 # initialize the correct shell for your machine to allow conda to work (see README for note on shell names)
 conda init bash
 
-# activate the main conda environment
-conda activate jump_sc
-
 # convert all notebooks to python files into the nbconverted folder
-jupyter nbconvert --to python --output-dir=nbconverted/ *.ipynb
+conda run -n jump_sc jupyter nbconvert --to python \
+    --output-dir=nbconverted/ \
+    *.ipynb
 
-# run the python scripts in order
-python nbconverted/0.generate_jump_dataset_manifest.py
-python nbconverted/1.download_jump_single_cell_profiles.py
+# download the jump manifest data
+conda run -n jump_sc python nbconverted/0.generate_jump_dataset_manifest.py
+
+# process the plate data using cytotable
+conda run -n jump_sc python 1.process_JUMP_plates_with_CytoTable.py
