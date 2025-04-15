f69499be8cd5b3e313442f07b9ca854def85c54d
Cameron Mattson <camo@oak.ucdenver.pvt>
2025-04-15 16:14:27 -0600

    Renamed anomalyze training folder and added functionality to compute anomaly data from pretrained anomalyze models.

---
+++ 2.evaluate_data/compute_sc_anomaly_data/compute_all_sc_anomaly_data.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This script calls python code to compute single-cell anomaly data with all datasets.
+
+conda init bash
+conda activate jump_sc
+
+# Get the root directory
+git_root=$(git rev-parse --show-toplevel)
+
+py_path="nbconverted"
+
+jupyter nbconvert --to python --output-dir="${py_path}/" *.ipynb
+
+iso_forest_paths=f"${git_root}/2.evaluate_data/train_sc_anomalyze_models/isolation_forest_models"
+
+# Get the normalized data path (with multiple plates)
+plate_paths=(
+    "${git_root}/big_drive/feature_selected_sc_qc_data"
+    "${git_root}/big_drive/normalized_sc_qc_data"
+    "${git_root}/big_drive/feature_selected_sc_data"
+    "${git_root}/big_drive/normalized_sc_data"
+)
+
+for plate_dir in "${plate_paths[@]}"; do
+
+    if [ -d "$plate_dir" ]; then
+
+        iso_forest_path="$iso_forest_path/$(basename "$plate_dir")_isolation_forest.joblib"
+
+        echo -e "\nSampling from $plate_dir"
+
+        /usr/bin/time -v python3 "$py_path/compute_sc_outlier_data.py" "$plate_dir" "$iso_forest_path"
+
+    else
+        echo "Error: '$plate_dir' is not a directory."
+        return 1
+    fi
+
+done
---
+++ 2.evaluate_data/compute_sc_anomaly_data/nbconverted/compute_sc_anomaly_data.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Identify single cell anomalies
+# In this analysis we compute single-cell anomaly data with anomalyze
+
+# In[ ]:
+
+
+import pathlib
+import sys
+
+import dask.dataframe as dd
+import joblib
+import pandas as pd
+from sklearn.ensemble import IsolationForest
+
+
+# ## Identify Anomaly Data
+
+# In[ ]:
+
+
+def compute_sc_outlier_data(
+    _scdf: pd.DataFrame,
+) -> pd.DataFrame:
+    """
+    Compute single-cell outlier data.
+
+    Parameters
+    ----------
+    _scdf: Single cell profile data containing morphology features and metadata.
+    """
+
+    # Isolation forest reference:
+    # https://ieeexplore.ieee.org/document/4781136
+    isofor = IsolationForest(n_estimators=1_000, random_state=0, n_jobs=-1)
+    _scdf = _scdf.assign(Result_inlier=isofor.fit_predict(_scdf[feat_cols].copy()))
+    _scdf = _scdf[meta_cols].assign(
+        Result_anomaly_score=isofor.decision_function(_scdf[feat_cols].copy())
+    )
+
+    _scdf.sort_values(by="Result_anomaly_score", ascending=True, inplace=True)
+
+    return _scdf[
+        meta_cols
+        + [
+            "Result_inlier",
+            "Result_anomaly_score",
+        ]
+    ]
+
+
+# ## Define inputs and outputs
+
+# In[ ]:
+
+
+sc_data_path = pathlib.Path(sys.argv[1].resolve(strict=True))
+scddf = dd.read_parquet(sc_data_path / "*.parquet")
+
+iso_forest = joblib.load(pathlib.Path(sys.argv[2]).resolve(strict=True))
+
+anomaly_data_path = pathlib.Path("sc_anomaly_data")
+anomaly_data_path.mkdir(parents=True, exist_ok=True)
+
+
+# In[ ]:
+
+
+feat_cols = iso_forest.feature_names_in_
+meta_cols = [col for col in scddf.columns if "Metadata" in col]
+
+meta_dict = {col: scddf[col].dtype for col in meta_cols}
+meta_dict["Result_inlier"] = "i1"  # int8
+meta_dict["Result_anomaly_score"] = "f8"  # float64
+
+outlier_ddf = scddf.map_partitions(compute_sc_outlier_data, meta=meta_dict)
+outlier_ddf.to_parquet(
+    anomaly_data_path / sc_data_path.name / "_anomaly_data.parquet", write_index=False
+)
+
