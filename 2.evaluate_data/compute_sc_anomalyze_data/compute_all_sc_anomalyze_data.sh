54f9a1b1b5e62f5d4dcda77eaf63fabec8bf55cd
Cameron Mattson <camo@oak.ucdenver.pvt>
2025-06-20 11:11:11 -0600

    Added code to sample and compute feature importances

--- 2.evaluate_data/compute_sc_anomalyze_data/compute_all_sc_anomalyze_data.sh
+++ 2.evaluate_data/compute_sc_anomalyze_data/compute_all_sc_anomalyze_data.sh
@@ -13,7 +13,6 @@ py_path="nbconverted"
 jupyter nbconvert --to python --output-dir="${py_path}/" ./*.ipynb
 
 iso_forest_paths="${git_root}/2.evaluate_data/train_sc_anomalyze_models/isolation_forest_models"
-anomaly_data_path="${git_root}/big_drive/sc_anomaly_data"
 feature_importance_data_path="${git_root}/big_drive/sc_feature_importance_data"
 
 # Get the single-cell data path (with multiple plates)
@@ -34,8 +33,6 @@ for plate_dir in "${plate_paths[@]}"; do
             if [ -f "$file" ]; then
 
                 iso_forest_path="$iso_forest_paths/$(basename "$plate_dir")_isolation_forest.joblib"
-
-                /usr/bin/time -v python3 "$py_path/compute_sc_anomaly_data.py" "$file" "$iso_forest_path" "$anomaly_data_path"
 
                 /usr/bin/time -v python3 "$py_path/compute_sc_feature_importance_data.py" "$file" "$iso_forest_path" "$feature_importance_data_path"
 
