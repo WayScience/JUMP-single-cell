596046dd9e763e2a1bc9b24e26f0dfb6e61323ab
Cameron Mattson <camo@oak.ucdenver.pvt>
2025-08-27 10:42:10 -0600

    Added code to save crop data and visualize the distribution of nuclei crop sizes

--- 3.analyze_data/predict_nuclear_speckle_images/nuclei_dimension_analysis/nbconverted/save_coords_plate.py
+++ 3.analyze_data/predict_nuclear_speckle_images/nuclei_dimension_analysis/nbconverted/save_coords_plate.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Compute Plate Crop Dimensions
+# # Compute Plate Crop Dimension Data
 
 # In[ ]:
 
@@ -10,6 +10,7 @@ import pathlib
 import sys
 
 import pandas as pd
+import pyarrow.parquet as pq
 
 
 # ## Find the root of the git repo on the host system
@@ -42,10 +43,6 @@ if root_dir is None:
 
 metadata_path = pathlib.Path(sys.argv[1])
 
-# Did not remove nans from parquet files
-# so downstream code can decide how to handle this data.
-platedf = pd.read_parquet(metadata_path)
-
 
 # # Outputs
 
@@ -56,12 +53,6 @@ crop_dimensions_data_path = root_dir / f"big_drive/nuclei_crop_dimensions_jump"
 
 crop_dimensions_data_path.mkdir(parents=True, exist_ok=True)
 
-plate_dimensions_data_path = (
-    root_dir
-    / crop_dimensions_data_path
-    / f"{metadata_path.stem}_nuclei_crop_dimensions.parquet"
-)
-
 
 # ## Determine Important Columns
 # Columns to save
@@ -69,39 +60,45 @@ plate_dimensions_data_path = (
 # In[ ]:
 
 
-meta_cols = list(platedf.columns[platedf.columns.str.contains("Metadata")])
-bounding_box_coord_cols = [
-    "Nuclei_AreaShape_BoundingBoxMaximum_X",
-    "Nuclei_AreaShape_BoundingBoxMinimum_X",
-    "Nuclei_AreaShape_BoundingBoxMaximum_Y",
-    "Nuclei_AreaShape_BoundingBoxMinimum_Y",
-]
+for i, batch in enumerate(
+    pq.ParquetFile(metadata_path).iter_batches(batch_size=220_000)
+):
+    # Did not remove nans from parquet files
+    # so downstream code can decide how to handle this data.
+    platedf = batch.to_pandas()
 
-platedf["Nuclei_AreaShape_BoundingBoxDelta_X"] = (
-    platedf["Nuclei_AreaShape_BoundingBoxMaximum_X"]
-    - platedf["Nuclei_AreaShape_BoundingBoxMinimum_X"]
-)
-
-
-platedf["Nuclei_AreaShape_BoundingBoxDelta_Y"] = (
-    platedf["Nuclei_AreaShape_BoundingBoxMaximum_Y"]
-    - platedf["Nuclei_AreaShape_BoundingBoxMinimum_Y"]
-)
-
-
-# ## Save Crop Dimensions
-
-# In[ ]:
-
-
-kept_cols = (
-    [
-        "Nuclei_AreaShape_BoundingBoxDelta_Y",
-        "Nuclei_AreaShape_BoundingBoxDelta_X",
+    meta_cols = list(platedf.columns[platedf.columns.str.contains("Metadata")])
+    bounding_box_coord_cols = [
+        "Nuclei_AreaShape_BoundingBoxMaximum_X",
+        "Nuclei_AreaShape_BoundingBoxMinimum_X",
+        "Nuclei_AreaShape_BoundingBoxMaximum_Y",
+        "Nuclei_AreaShape_BoundingBoxMinimum_Y",
     ]
-    + meta_cols
-    + bounding_box_coord_cols
-)
 
-platedf[kept_cols].to_parquet(plate_dimensions_data_path)
+    platedf["Nuclei_AreaShape_BoundingBoxDelta_X"] = (
+        platedf["Nuclei_AreaShape_BoundingBoxMaximum_X"]
+        - platedf["Nuclei_AreaShape_BoundingBoxMinimum_X"]
+    )
+
+    platedf["Nuclei_AreaShape_BoundingBoxDelta_Y"] = (
+        platedf["Nuclei_AreaShape_BoundingBoxMaximum_Y"]
+        - platedf["Nuclei_AreaShape_BoundingBoxMinimum_Y"]
+    )
+
+    kept_cols = (
+        [
+            "Nuclei_AreaShape_BoundingBoxDelta_Y",
+            "Nuclei_AreaShape_BoundingBoxDelta_X",
+        ]
+        + meta_cols
+        + bounding_box_coord_cols
+    )
+
+    plate_dimensions_data_path = (
+        root_dir
+        / crop_dimensions_data_path
+        / f"{metadata_path.stem}_nuclei_crop_dimensions_{i:02d}.parquet"
+    )
+
+    platedf[kept_cols].to_parquet(plate_dimensions_data_path)
 
---
+++ 3.analyze_data/predict_nuclear_speckle_images/nuclei_dimension_analysis/nbconverted/visualize_nuclei_crop_distribution.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Visualize Nuclei Crop Distribution
+
+# In[1]:
+
+
+import pathlib
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+# ## Find the root of the git repo on the host system
+
+# In[2]:
+
+
+# Get the current working directory
+cwd = pathlib.Path.cwd()
+
+if (cwd / ".git").is_dir():
+    root_dir = cwd
+
+else:
+    root_dir = None
+    for parent in cwd.parents:
+        if (parent / ".git").is_dir():
+            root_dir = parent
+            break
+
+# Check if a Git root directory was found
+if root_dir is None:
+    raise FileNotFoundError("No Git root directory found.")
+
+
+# # Inputs
+
+# In[3]:
+
+
+crop_dimensions_data_paths = list(
+    (root_dir / "big_drive/nuclei_crop_dimensions_jump")
+    .resolve(strict=True)
+    .rglob("*.parquet")
+)
+cropdimdf = pd.concat(
+    [pd.read_parquet(platecropdf) for platecropdf in crop_dimensions_data_paths], axis=0
+)
+
+
+# # Outputs
+
+# In[4]:
+
+
+nuclei_crop_figure_path = pathlib.Path("nuclei_crop_distribution_figure")
+nuclei_crop_figure_path.mkdir(parents=True, exist_ok=True)
+
+
+# # Plot Sample Distribution of Nuclei Crops
+
+# In[5]:
+
+
+g = sns.jointplot(
+    data=cropdimdf.sample(n=15_000, random_state=0),
+    x="Nuclei_AreaShape_BoundingBoxDelta_X",
+    y="Nuclei_AreaShape_BoundingBoxDelta_Y",
+    kind="kde",
+)
+g.fig.set_size_inches(18, 10)
+g.set_axis_labels(
+    "Bounding Box Height (Pixels)", "Bounding Box Length (Pixels)", fontsize=13
+)
+g.fig.suptitle("Sample Distribution of Bounding Box Sizes", fontsize=16)
+
+g.ax_joint.tick_params(axis="x", labelsize=12)
+g.ax_joint.tick_params(axis="y", labelsize=12)
+g.ax_joint.legend(fontsize=12)
+
+plt.savefig(nuclei_crop_figure_path / "nuclei_crop_distributions.png")
+plt.show()
+
